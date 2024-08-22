37d65bc95d3de535e151e331d2c790c264b03ae1
hendisantika <hendisantika@yahoo.co.id>
2024-08-22 13:30:19 +0700

    Add client Object 🫸🌀✏️📗🐧🐳⬆

--- internal/http-client/http_client.go
+++ internal/http-client/http_client.go
@@ -1,7 +1,14 @@
 package http_client
 
+import "net/http"
+
 type AgeResponse struct {
 	Count uint64
 	Name  string
 	Age   int
+}
+
+type client struct {
+	baseUrl    string
+	httpClient *http.Client
 }
