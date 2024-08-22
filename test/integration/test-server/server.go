10b7745018de9a1d7922f8cb70e6c4fa161ae63b
hendisantika <hendisantika@yahoo.co.id>
2024-08-22 13:37:10 +0700

    Add getRoot func 🫸🌀✏️📗🐧🐳⬆

--- test/integration/test-server/server.go
+++ test/integration/test-server/server.go
@@ -1,7 +1,28 @@
 package test_server
 
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
 type AgeResponse struct {
 	Count uint64
 	Name  string
 	Age   int
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	ageResponse := AgeResponse{
+		Count: 1000,
+		Name:  name,
+		Age:   62,
+	}
+	json, err := json.Marshal(ageResponse)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	io.WriteString(w, string(json))
 }
