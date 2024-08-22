4b2382e4cdcccbdd1e96c5d85590cff191aa80df
hendisantika <hendisantika@yahoo.co.id>
2024-08-22 13:31:42 +0700

    Add main func 🫸🌀✏️📗🐧🐳⬆

---
+++ cmd/test_client.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	http_client "go-testcontainer-mock-api/internal/http-client"
+	"os"
+)
+
+func main() {
+	client, err := http_client.NewClient("https://api.agify.io")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	res, err := client.GetAge("Yuji")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(res)
+
+}
